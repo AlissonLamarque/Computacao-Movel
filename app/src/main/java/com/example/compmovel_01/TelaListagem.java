5d3639d564794a241375dfaaf93af3473655051b
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-02-25 21:44:41 -0300

    commit

--- app/src/main/java/com/example/compmovel_01/TelaListagem.java
+++ app/src/main/java/com/example/compmovel_01/TelaListagem.java
@@ -1,6 +1,9 @@
 package com.example.compmovel_01;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TelaListagem extends AppCompatActivity {
+import java.util.List;
 
+public class TelaListagem extends AppCompatActivity {
+    private ListView listView;
+    private AlunoDao dao;
+    private List<Aluno> alunos;
+
+    @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela_listagem);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        listView = findViewById(R.id.lista_alunos);
+        dao = new AlunoDao(this);
+        alunos = dao.obterTodos();
+
+        ArrayAdapter<Aluno> adaptador = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
+        listView.setAdapter(adaptador);
     }
 }
