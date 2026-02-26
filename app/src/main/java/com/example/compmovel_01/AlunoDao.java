c97d51a6677eb4450d11eeb2678b77c456504c70
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-02-25 21:24:59 -0300

    commit

---
+++ app/src/main/java/com/example/compmovel_01/AlunoDao.java
@@ -0,0 +1,46 @@
+package com.example.compmovel_01;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlunoDao {
+    private Conexao conexao;
+    private SQLiteDatabase banco;
+
+    public AlunoDao(Context context){
+        conexao = new Conexao(context);
+        banco = conexao.getWritableDatabase();
+    }
+
+    public long inserir(Aluno aluno){
+        ContentValues values = new ContentValues();
+        values.put("nome", aluno.getNome());
+        values.put("cpf", aluno.getCpf());
+        values.put("telefone", aluno.getTelefone());
+        values.put("endereco", aluno.getEndereco());
+        values.put("curso", aluno.getCurso());
+        return banco.insert("aluno", null, values);
+    }
+
+    public List<Aluno> obterTodos(){
+        List<Aluno> alunos = new ArrayList<>();
+        Cursor cursor = banco.query("aluno", new String[]{"id", "nome", "cpf", "telefone", "endereco", "curso"},
+                null, null, null, null, null);
+        while (cursor.moveToNext()){
+            Aluno a = new Aluno();
+            a.setId(cursor.getInt(0));
+            a.setNome(cursor.getString(1));
+            a.setCpf(cursor.getString(2));
+            a.setTelefone(cursor.getString(3));
+            a.setEndereco(cursor.getString(4));
+            a.setCurso(cursor.getString(5));
+            alunos.add(a);
+        }
+        return alunos;
+    }
+}
---
+++ app/src/main/java/com/example/compmovel_01/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.compmovel_01;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText nome;
+    private EditText cpf;
+    private EditText telefone;
+    private EditText endereco;
+    private EditText curso;
+    private AlunoDao dao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nome = findViewById(R.id.editNome);
+        cpf = findViewById(R.id.editCpf);
+        telefone = findViewById(R.id.editTelefone);
+        endereco = findViewById(R.id.editEndereco);
+        curso = findViewById(R.id.editCurso);
+
+        dao = new AlunoDao(this);
+    }
+
+    public void salvar(View view){
+        Aluno a = new Aluno();
+        a.setNome(nome.getText().toString());
+        a.setCpf(cpf.getText().toString());
+        a.setTelefone(telefone.getText().toString());
+        a.setEndereco(endereco.getText().toString());
+        a.setCurso(curso.getText().toString());
+        long id = dao.inserir(a);
+        Toast.makeText(this, "Aluno inserido com id: "+id, Toast.LENGTH_SHORT).show();
+    }
+}
