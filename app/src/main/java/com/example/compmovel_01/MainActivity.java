8d771dcca5e8e0da79c566b65a8af8099af6c170
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-03-04 21:07:34 -0300

    Atualizar e Excluir

--- app/src/main/java/com/example/compmovel_01/MainActivity.java
+++ app/src/main/java/com/example/compmovel_01/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.compmovel_01;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText endereco;
     private EditText curso;
     private AlunoDao dao;
+    private Aluno aluno = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +33,39 @@ public class MainActivity extends AppCompatActivity {
         curso = findViewById(R.id.editCurso);
 
         dao = new AlunoDao(this);
+
+        Intent it = getIntent(); //pega intenção
+        if(it.hasExtra("aluno")){
+            aluno = (Aluno) it.getSerializableExtra("aluno");
+            nome.setText(aluno.getNome().toString());
+            cpf.setText(aluno.getCpf());
+            telefone.setText(aluno.getTelefone());
+            endereco.setText(aluno.getEndereco());
+            curso.setText(aluno.getCurso());
+        }
     }
 
     public void salvar(View view){
-        Aluno a = new Aluno();
-        a.setNome(nome.getText().toString());
-        a.setCpf(cpf.getText().toString());
-        a.setTelefone(telefone.getText().toString());
-        a.setEndereco(endereco.getText().toString());
-        a.setCurso(curso.getText().toString());
-        long id = dao.inserir(a);
-        Toast.makeText(this, "Aluno inserido com id: "+id, Toast.LENGTH_SHORT).show();
+        if(aluno==null) {
+            Aluno a = new Aluno();
+            a.setNome(nome.getText().toString());
+            a.setCpf(cpf.getText().toString());
+            a.setTelefone(telefone.getText().toString());
+            a.setEndereco(endereco.getText().toString());
+            a.setCurso(curso.getText().toString());
+            long id = dao.inserir(a);
+            Toast.makeText(this, "Aluno inserido com id: "+id, Toast.LENGTH_SHORT).show();
+        } else {
+            aluno.setNome(nome.getText().toString());
+            aluno.setCpf(cpf.getText().toString());
+            aluno.setTelefone(telefone.getText().toString());
+            dao.atualizar(aluno);
+            Toast.makeText(this,"Aluno Atualizado!! com id: ", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void irParaListar(View view){
+        Intent intent = new Intent(this, TelaListagem.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/compmovel_01/TelaListagem.java
+++ app/src/main/java/com/example/compmovel_01/TelaListagem.java
@@ -1,7 +1,15 @@
 package com.example.compmovel_01;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,12 +19,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TelaListagem extends AppCompatActivity {
     private ListView listView;
     private AlunoDao dao;
     private List<Aluno> alunos;
+    private List<Aluno> alunosFiltrados = new ArrayList<>();
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -25,10 +35,62 @@ public class TelaListagem extends AppCompatActivity {
         setContentView(R.layout.activity_tela_listagem);
 
         listView = findViewById(R.id.lista_alunos);
+        registerForContextMenu(listView);
+
         dao = new AlunoDao(this);
         alunos = dao.obterTodos();
+        alunosFiltrados.addAll(alunos);
 
         ArrayAdapter<Aluno> adaptador = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
+        listView.setAdapter(adaptador);
+    }
+
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater i = getMenuInflater();
+        i.inflate(R.menu.menu_contexto, menu);
+    }
+
+    public void voltarParaCadastro(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void excluir(MenuItem item){
+        AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        final Aluno alunoExcluir = alunosFiltrados.get(menuInfo.position);
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Atenção")
+                .setMessage("Realmente deseja excluir o aluno?")
+                .setNegativeButton("NÃO",null)
+                .setPositiveButton("SIM", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        alunosFiltrados.remove(alunoExcluir);
+                        alunos.remove(alunoExcluir);
+                        dao.excluir(alunoExcluir);
+                        listView.invalidateViews();
+                    }
+                } ).create();
+        dialog.show();
+    }
+
+    public void atualizar(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        final Aluno alunoAtualizar = alunosFiltrados.get(menuInfo.position);
+        Intent it = new Intent(this, MainActivity.class);
+        it.putExtra("aluno", alunoAtualizar);
+        startActivity(it);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        alunos = dao.obterTodos();
+        alunosFiltrados.clear();
+        alunosFiltrados.addAll(alunos);
+        ArrayAdapter<Aluno> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunosFiltrados);
         listView.setAdapter(adaptador);
     }
 }
